c2180785f325bc9a759ceb854a8c9a6c1548c267
millerb08 <miller.steffens@gmail.com>
2020-04-30 13:34:53 -0500

    APP ENV

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
+if(getenv("APP_ENV") != "production" ){
+  $dotenv->load();
+}
+$dotenv->required(['REPOSITORY', 'SITE_KEY', 'SECRET_KEY', 'SMTP_DEBUG', 'SMTP_HOST', 'SMTP_PORT', 'SMTP_SECURE', 'SMTP_AUTH', 'SMTP_USERNAME', 'SMTP_PASSWORD', 'SMTP_FROM', 'MAILTO_EMAIL', 'MAILTO_NAME']);
+
